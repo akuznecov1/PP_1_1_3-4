883e9edbf96bb25e192d6a67144fb4d2ffa1d3b4
[akuznecov1] <kuznecov0996@gmail.com>
2022-09-19 12:44:16 +0500

    lolkek

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,17 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public static Connection getConnection() throws ClassNotFoundException, SQLException {
+        String userName = "root";
+        String password = "root";
+        String connectionURL = "jdbc:mysql://localhost:3306/kek";
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection(connectionURL, userName, password);
+        return connection;
+    }
 }
+
